58d5b8a13655379dbd8daa3d8a6b969fbe62f844
palomarodrigs <palomarodrigues.0013@gmail.com>
2024-08-06 10:48:42 -0300

    style: adjust responsive in CountdownContainer and Separator components

--- src/pages/Home/components/Countdown/styles.ts
+++ src/pages/Home/components/Countdown/styles.ts
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export const CountdownContainer = styled.div`
   font-family: 'Roboto Mono', monospace;
-  font-size: 10rem;
-  line-height: 8rem;
+  font-size: 3rem;
+  line-height: 2rem;
 
   color: ${(props) => props.theme['gary-100']};
 
@@ -15,15 +15,24 @@ export const CountdownContainer = styled.div`
     padding: 2rem 1rem;
     border-radius: 8px;
   }
+
+  @media (min-width: 768px) {
+    font-size: 10rem;
+    line-height: 8rem;
+  }
 `
 
 export const Separator = styled.div`
   display: flex;
   justify-content: center;
 
-  width: 4rem;
+  width: 2rem;
   padding: 2rem 0;
 
   overflow: hidden;
   color: ${(props) => props.theme['green-500']};
+
+  @media (min-width: 768px) {
+    width: 4rem;
+  }
 `
